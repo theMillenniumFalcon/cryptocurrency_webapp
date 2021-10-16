89573d6a6f6dee2f82e7df1fd82dda5295cf9de2
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-10-17 03:10:20 +0530

    navbar_working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from 'antd'
+import Navbar from "./components/navbar/Navbar"
+import './App.css'
+
 function App() {
   return (
-    <div>
-      Hello
+    <div className="app">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+        
+      </div>
     </div>
   );
 }
