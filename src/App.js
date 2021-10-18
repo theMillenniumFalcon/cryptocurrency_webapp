fb5d44a426846ead798e1351a0e6ff54d2071c7c
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-10-19 02:05:57 +0530

    Reinitialized-the-app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Layout, Typography, Space } from 'antd'
-import Navbar from "./components/navbar/Navbar"
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <div className="navbar">
-        <Navbar/>
-      </div>
-      <div className="main">
-
-      </div>
-      <div className="footer">
-        
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
